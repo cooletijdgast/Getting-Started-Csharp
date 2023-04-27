4275907bca1db55ac2c460c3054850c3509ed8c9
larsbrinker <larsbrinker2000@gmail.com>
2023-04-27 11:19:22 +0200

    Added boolean exercise

---
+++ ConsoleApp1/ConsoleApp1/boolean.cs
@@ -0,0 +1,40 @@
+﻿//int coinFlip = new Random().Next(0, 2);
+//Console.WriteLine((coinFlip > 0 ? "Heads" : "Tails" ));
+
+
+string permission = "Manager"; //Admin | Manager
+int level = 23;
+
+Boolean admin = permission.Contains("Admin");
+Boolean manager = permission.Contains("Manager");
+Boolean isHighestLevel = level > 55;
+Boolean isLowestLevel = level >= 20;
+Boolean isLowerThan55 = level <= 55;
+Boolean isLowerThan20 = level < 20;
+
+if (admin)
+{
+    if (isHighestLevel)
+    {
+        Console.WriteLine("Welcome, Super Admin User");
+    }
+    else if (isLowerThan55)
+    {
+        Console.WriteLine("Welcome, admin user");
+    }
+}
+else if (manager)
+{
+    if (isLowestLevel)
+    {
+        Console.WriteLine("Contact an admin for access");
+    }
+    else if (isLowerThan20)
+    {
+        Console.WriteLine("You do not have suffiecient priviliges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
--- ConsoleApp1/ConsoleApp1/coinflip.cs
+++
@@ -1,2 +0,0 @@
-﻿int coinFlip = new Random().Next(0, 2);
-Console.WriteLine((coinFlip > 0 ? "Heads" : "Tails" ));
